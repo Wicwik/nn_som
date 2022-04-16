94f2478a0883f7a644dd8f08bab70be85e046d6f
Robert Belanec <belanecrobert22@gmail.com>
2022-04-16 09:55:23 +0200

    working fast

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from util import *
 def L_max(i, j, axis=0):
     return np.max(np.abs(i - j), axis=axis)
 
-def L_1(i, j):
-    return float(np.sum(abs(i - j)))
+def L_1(i, j, axis=0):
+    # print(j,axis)
+    return np.sum(abs(i - j), axis=axis)
 
-def L_2(i, j):
-    return np.sqrt(np.sum(np.power(i - j, 2)))
+def L_2(i, j, axis=0):
+    return np.sqrt(np.sum(np.power(i - j, 2), axis=axis))
 
 # Test norms
 # A = np.array([1,2,3,10])
@@ -17,10 +18,10 @@ def L_2(i, j):
 # print(np.linalg.norm(A - B, ord=1), L_1(A,B))
 
 # Test input
-# inputs = np.random.rand(2, 250)
-# inputs[1,:] += 0.5 * inputs[0,:]
+inputs = np.random.rand(2, 250)
+inputs[1,:] += 0.5 * inputs[0,:]
 
-inputs = np.loadtxt('seeds_dataset.txt').T[:-1]
+# inputs = np.loadtxt('seeds_dataset.txt').T[:-1]
 
 (dim_in, count) = inputs.shape
 
@@ -30,8 +31,11 @@ inputs = np.loadtxt('seeds_dataset.txt').T[:-1]
 rows = 13
 cols = 17
 
+# rows = 23
+# cols = 27
+
 # Choose grid distance metric - L_1 / L_2 / L_max
-grid_metric = L_max
+grid_metric = L_1
 
 # Some heuristics for choosing initial lambda
 top_left = np.array((0, 0))
@@ -39,4 +43,4 @@ bottom_right = np.array((rows-1, cols-1))
 lambda_s = grid_metric(top_left, bottom_right) * 0.5
 
 model = SOM(dim_in, rows, cols, inputs)
-model.train(inputs, eps=10, alpha_s=0.5, alpha_f=0.01, lambda_s=lambda_s, lambda_f=1, discrete_neighborhood=True, grid_metric=grid_metric)
+model.train(inputs, eps=100, alpha_s=0.5, alpha_f=0.01, lambda_s=lambda_s, lambda_f=1, discrete_neighborhood=False, grid_metric=grid_metric)
--- som.py
+++ som.py
@@ -15,9 +15,11 @@ class SOM():
 
         self.weights = np.random.rand(self.n_rows, self.n_cols, self.dim_in)
 
+        # create weights indexes for mesh distance calculation
+        self.w_idx = np.stack(np.unravel_index(np.arange(self.n_rows * self.n_cols, dtype=int).reshape(self.n_rows, self.n_cols), 
+            (self.n_rows, self.n_cols)), axis=2)
+
         if inputs is not None:
-            # "Fill" the input space with neurons - scale and shift neurons to inputs' distribution.
-            # Note: SOM will train even without it, but it helps.
             ma = np.max(inputs, axis=1)
             mi = np.min(inputs, axis=1)
 
@@ -29,31 +31,17 @@ class SOM():
         Find winner neuron and return its coordinates in grid (i.e. its "index").
         Iterate over all neurons and find the neuron with the lowest distance to input x (np.linalg.norm).
         '''
-        win_r, win_c = -1, -1
 
-        for r in range(self.n_rows):
-            for c in range(self.n_cols):
-                norm = np.linalg.norm(self.weights[r, c] - x)
-                win_norm = np.linalg.norm(self.weights[win_r, win_c] - x)
-                if win_r == -1 and win_c == -1:
-                    win_r = r
-                    win_c = c
-                    
-                if norm < win_norm:
-                    win_r = r
-                    win_c = c
+        return np.unravel_index(np.argmin(np.linalg.norm(self.weights - x, axis=2)), (self.n_rows, self.n_cols))
 
 
-        return win_r, win_c
-
-
-    def train(self,
-              inputs,   # Matrix of inputs - each column is one input vector
-              eps=100,  # Number of epochs
+    def train(self, 
+              inputs,                                                  # Matrix of inputs - each column is one input vector
+              eps=100,                                                 # Number of epochs
               alpha_s=0.01, alpha_f=0.001, lambda_s=None, lambda_f=1,  # Start & end values for alpha & lambda
-              discrete_neighborhood=True,  # Use discrete or continuous (gaussian) neighborhood function
-              grid_metric=(lambda u,v:0),  # Grid distance metric
-              live_plot=False, live_plot_interval=10  # Draw plots dring training process
+              discrete_neighborhood=True,                              # Use discrete or continuous (gaussian) neighborhood function
+              grid_metric=(lambda u,v:0),                              # Grid distance metric
+              live_plot=False, live_plot_interval=10                   # Draw plots dring training process
              ):
 
         (_, count) = inputs.shape
@@ -65,25 +53,23 @@ class SOM():
             redraw()
 
         for ep in range(eps):
-            alpha_t  = alpha_s*(alpha_f/alpha_s)**(ep/(eps-1))  # FIXME
-            lambda_t = lambda_s*(lambda_f/lambda_s)**(ep/(eps-1))  # FIXME
+            alpha_t  = alpha_s*(alpha_f/alpha_s)**(ep/(eps-1))
+            lambda_t = lambda_s*(lambda_f/lambda_s)**(ep/(eps-1))
 
             for idx in np.random.permutation(count):
                 x = inputs[:, idx]
 
                 win_r, win_c = self.winner(x)
 
-                # Use "d = grid_metric(vector_a, vector_b)" for grid distance
-                # Use discrete neighborhood
-
-                for r in range(self.n_rows):
-                    for c in range(self.n_cols):
-                        # ...
-                        d = grid_metric(np.array((r, c)), np.array((win_r, win_c)))
-                        h_t = 1 if d < lambda_t else 0
-
-                        self.weights[r, c] += alpha_t*(x-self.weights[r, c])*h_t  # FIXME
-
+                if discrete_neighborhood:
+                    # this will return a map of distances after which we discreetly move only the < lambda_t
+                    d = grid_metric(self.w_idx, np.array((win_r, win_c)), axis=(len(self.w_idx.shape)-1))
+                    h_t = d < lambda_t
+                    self.weights[h_t] += alpha_t*(x-self.weights[h_t])
+                else:
+                    d = grid_metric(self.w_idx, np.array((win_r, win_c)), axis=(len(self.w_idx.shape)-1))
+                    h_t = np.exp(-np.power(d,2)/np.power(lambda_t,2)).reshape(self.n_rows, self.n_cols, 1)
+                    self.weights += alpha_t*(x-self.weights)*h_t
 
             print('Ep {:3d}/{:3d}:  alpha_t = {:.3f}, lambda_t = {:.3f}'
                   .format(ep+1, eps, alpha_t, lambda_t))
